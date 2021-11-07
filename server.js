056782c3ca062b11cf968281bd947916204e06b9
Nitish <nitish_bhatia@intuit.com>
2021-11-07 22:10:41 +0530

    responding with hostname

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = 'Jai Jai Ram\n'; 
+const message = `Jai Jai Ram from ${hostname()}\n`;
 const port = 8080; 
 const server = http.createServer((req, res) => { 
   res.statusCode = 200;
