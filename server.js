a99bf9b9cd089ff99139e506300e25cd5f3dff35
Nitish <nitish_bhatia@intuit.com>
2021-12-30 14:59:02 +0530

    chore: application listening to https traffic

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const { hostname } = require('os');
 const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
 const STACK_NAME = process.env.STACK_NAME || "Unknown Stack";
 const message = `Jai Jai Ram from ${hostname()} in ${STACK_NAME}\n`;
 const port = 8080; 
+const httpsPort = 8443;
+// using self signed certificate in ec2 instance generated at the time of launching ec2 instance from deployment script
+const httpsKey = '../keys/key.pem';
+const httpsCert = '../keys/cert.pem';
+if (fs.existsSync(httpsKey) && fs.existsSync(httpsCert)) { 
+  console.log('Starting https server')
+  const options = { key: fs.readFileSync(httpsKey), cert: fs.readFileSync(httpsCert) };
+  const server = https.createServer(options, (req, res) => { 
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(message);
+  });
+  server.listen(httpsPort, hostname, () => {
+    console.log(`Server running at https://${hostname()}:${httpsPort}/`);
+  });
+}
+
+console.log('Starting http server') 
 const server = http.createServer((req, res) => { 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
